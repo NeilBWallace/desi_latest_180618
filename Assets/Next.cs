b85b1da68064258b70622fac9a098f720f489b2d
DevOpHBLICT <AppDevelopment@hblict.nhs.uk>
2018-06-27 14:04:02 +0100

    'sfsdf'

---
+++ Assets/Next.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.SceneManagement;
+public class Next : MonoBehaviour {
+    public void NextPage()
+    {
+        GameObject i;
+        i = GameObject.Find("Slots - Inventory(Clone)");
+
+        //		Image j =i.transform.GetChild (0).GetChild(0).GetChild(0).GetComponent<Image>();
+        //		Debug.Log ("sfsdf" + j.sprite.name);
+        //		j =i.transform.GetChild (1).GetChild(0).GetChild(0).GetComponent<Image>();
+        //		Debug.Log ("sfsdf" + j.sprite.name);
+        //		j =i.transform.GetChild (2).GetChild(0).GetChild(0).GetComponent<Image>();
+        //		Debug.Log ("sfsdf" + j.sprite.name);
+        //		j =i.transform.GetChild (3).GetChild(0).GetChild(0).GetComponent<Image>();
+        //		Debug.Log ("sfsdf" + j.sprite.name);
+        //		j =i.transform.GetChild (4).GetChild(0).GetChild(0).GetComponent<Image>();
+        //		Debug.Log ("sfsdf" + j.sprite.name);
+
+        int sceneID = SceneManager.GetActiveScene().buildIndex;
+
+
+        SceneManager.LoadScene(sceneID + 1);
+
+
+
+
+
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
