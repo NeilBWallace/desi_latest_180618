d718381dbf0412d9f50a8a26f2c85e57a818f4c4
DevOpHBLICT <AppDevelopment@hblict.nhs.uk>
2018-07-06 16:04:40 +0100

    'sfdsd'

---
+++ Assets/Back.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Back : MonoBehaviour {
+
+
+  public   void GoBack()
+    {
+        CanvasGroup c = GameObject.Find("Quickly").GetComponent<CanvasGroup>();
+        c.alpha = 0;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
