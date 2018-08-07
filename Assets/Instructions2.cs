176192b7820f43cf6be623b2abe688b6f737b1e1
DevOpHBLICT <AppDevelopment@hblict.nhs.uk>
2018-08-07 13:13:04 +0100

    'sdfs'

---
+++ Assets/Instructions2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Instructions2 : MonoBehaviour {
+
+    public void RunInstructions()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void RunInstructions2()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Quiz.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quiz : MonoBehaviour {
+
+
+    void GiveUsEnergy()
+    {
+      
+
+        CanvasGroup d = GameObject.Find("Energy_Text").GetComponent<CanvasGroup>();
+        d.alpha = 1;
+    }
+    void ToSee()
+    {
+
+    }
+
+    void ToHeal()
+    {
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
